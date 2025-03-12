ab1e2199ade0a92fbc0c5ac2fd2e7ecf90fe868e
lucaslorenzipucrs <lucas.lorenzi86@edu.pucrs.br>
2025-03-11 21:19:57 -0300

    Update App.java pelo repositorio remoto

--- projeto1a/src/App.java
+++ projeto1a/src/App.java
@@ -3,5 +3,6 @@ public class App {
     System.out.println("\nHello!");
     String version = System.getProperty("java.version");
     System.out.println("Running Java Version "+version+"\n");
+    System.out.println("Alteraçao Direto no repositorio remoto pelo Github\n");
     }
    }
